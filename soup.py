9880267b074e5ab6dcb9ebe91885597d8e37c1d5
Hengsheng-Liu <lhs7714@bu.edu>
2023-04-17 12:36:47 -0400

    able to fully parse the school website

--- soup.py
+++ soup.py
@@ -2,7 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup, NavigableString
 from urllib.parse import urljoin
-
+import re
 url = "https://www.bu.edu/academics/cas/courses/computer-science/"
 response = requests.get(url)
 
@@ -24,14 +24,20 @@ for link in page_links:
         pre_req = span.text.strip() if span and "Undergraduate Prerequisites" in span.text else None
         description_parts = [content for content in course.contents if isinstance(content, NavigableString) and content.strip()]
         description = " ".join(part.strip() for part in description_parts).replace("\n", "")
+        parsed_pre_req = re.split(r'[.!?;]', pre_req) if pre_req else None
+        parsed_pre_req = [s.strip() for s in parsed_pre_req if s.strip()] if parsed_pre_req else None
+        recommended = [s for s in parsed_pre_req if "recommended" in s] if parsed_pre_req else None
+        print(recommended)
         for a in course.find_all('a'):
             if a.get('href') != "http://www.bu.edu/hub/what-is-the-hub/":
                 link = urljoin(url, a.get('href')).strip()
                 course_data = {
                     'name': name,
-                    'prerequisites': pre_req,
+                    'prerequisites': parsed_pre_req,
                     'link': link,
-                    'description': description
+                    'description': description,
+                    'consent': True if parsed_pre_req and any(('consent of instructor' in s) or ('approved by the instructor' in s) for s in parsed_pre_req) else False,
+                    'recommended': recommended if recommended else None
                 }
                 convert_to_json.append(course_data)
 with open("output.json", "w") as outfile:
